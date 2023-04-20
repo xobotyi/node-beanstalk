8c34ba0b57edbad6ff0d19a6b7498fc7c1ff86d8
Zebra North <mrzebra@mrzebra.co.uk>
2023-04-20 11:01:14 +0100

    Undo indent change

--- src/Client.ts
+++ src/Client.ts
@@ -756,7 +756,7 @@ export class Client extends EventEmitter {
       throw new ClientError(
         ClientErrorCode.ErrPayloadTooBig,
         `${serializer ? 'Serialized payload' : 'Payload'} is too big,` +
-        ` maximum size is ${maxPayloadSize} bytes, got ${payloadBuffer.length}`
+          ` maximum size is ${maxPayloadSize} bytes, got ${payloadBuffer.length}`
       );
     }
 
